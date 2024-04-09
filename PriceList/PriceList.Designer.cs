a290218cbeefaf322f587dfbd9edf9079cf618be
one1erp <avigaile@one1.co.il>
2024-04-09 09:07:00 +0300

    Initial commit with additional files

---
+++ PriceList/PriceList.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace PriceList
+{
+    partial class PriceList
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radPageViewPage6 = new Telerik.WinControls.UI.RadPageViewPage();
+            this.radPageViewPage5 = new Telerik.WinControls.UI.RadPageViewPage();
+            this.radPageViewPage4 = new Telerik.WinControls.UI.RadPageViewPage();
+            this.radPageViewPage3 = new Telerik.WinControls.UI.RadPageViewPage();
+            this.radPageViewPage2 = new Telerik.WinControls.UI.RadPageViewPage();
+            this.radPageViewPage1 = new Telerik.WinControls.UI.RadPageViewPage();
+            this.radPageView1 = new Telerik.WinControls.UI.RadPageView();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.okBtn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.radPageView1)).BeginInit();
+            this.radPageView1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // radPageViewPage6
+            // 
+            this.radPageViewPage6.Location = new System.Drawing.Point(10, 34);
+            this.radPageViewPage6.Name = "radPageViewPage6";
+            this.radPageViewPage6.Size = new System.Drawing.Size(741, 201);
+            this.radPageViewPage6.Tag = "C";
+            this.radPageViewPage6.Text = "כימיה";
+            // 
+            // radPageViewPage5
+            // 
+            this.radPageViewPage5.Location = new System.Drawing.Point(10, 34);
+            this.radPageViewPage5.Name = "radPageViewPage5";
+            this.radPageViewPage5.Size = new System.Drawing.Size(841, 351);
+            this.radPageViewPage5.Tag = "S";
+            this.radPageViewPage5.Text = "שפכים";
+            // 
+            // radPageViewPage4
+            // 
+            this.radPageViewPage4.Location = new System.Drawing.Point(8, 31);
+            this.radPageViewPage4.Name = "radPageViewPage4";
+            this.radPageViewPage4.Size = new System.Drawing.Size(746, 219);
+            this.radPageViewPage4.Tag = "W";
+            this.radPageViewPage4.Text = "מים";
+            // 
+            // radPageViewPage3
+            // 
+            this.radPageViewPage3.Location = new System.Drawing.Point(10, 34);
+            this.radPageViewPage3.Name = "radPageViewPage3";
+            this.radPageViewPage3.Size = new System.Drawing.Size(841, 351);
+            this.radPageViewPage3.Tag = "F";
+            this.radPageViewPage3.Text = "מזון";
+            // 
+            // radPageViewPage2
+            // 
+            this.radPageViewPage2.Location = new System.Drawing.Point(10, 34);
+            this.radPageViewPage2.Name = "radPageViewPage2";
+            this.radPageViewPage2.Size = new System.Drawing.Size(841, 351);
+            this.radPageViewPage2.Tag = "P";
+            this.radPageViewPage2.Text = "פארמצבטיקה";
+            // 
+            // radPageViewPage1
+            // 
+            this.radPageViewPage1.Location = new System.Drawing.Point(10, 34);
+            this.radPageViewPage1.Name = "radPageViewPage1";
+            this.radPageViewPage1.Size = new System.Drawing.Size(741, 114);
+            this.radPageViewPage1.Tag = "ALL";
+            this.radPageViewPage1.Text = "כל הבדיקות";
+            // 
+            // radPageView1
+            // 
+            this.radPageView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.radPageView1.Controls.Add(this.radPageViewPage1);
+            this.radPageView1.Controls.Add(this.radPageViewPage2);
+            this.radPageView1.Controls.Add(this.radPageViewPage3);
+            this.radPageView1.Controls.Add(this.radPageViewPage4);
+            this.radPageView1.Controls.Add(this.radPageViewPage5);
+            this.radPageView1.Controls.Add(this.radPageViewPage6);
+            this.radPageView1.Location = new System.Drawing.Point(14, 62);
+            this.radPageView1.Name = "radPageView1";
+            this.radPageView1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.radPageView1.SelectedPage = this.radPageViewPage6;
+            this.radPageView1.Size = new System.Drawing.Size(762, 246);
+            this.radPageView1.TabIndex = 0;
+            this.radPageView1.Tag = "P";
+            this.radPageView1.Text = "tab";
+            this.radPageView1.UseCompatibleTextRendering = false;
+            ((Telerik.WinControls.UI.RadPageViewStripElement)(this.radPageView1.GetChildAt(0))).StripButtons = Telerik.WinControls.UI.StripViewButtons.Scroll;
+            ((Telerik.WinControls.UI.RadPageViewStripElement)(this.radPageView1.GetChildAt(0))).StripAlignment = Telerik.WinControls.UI.StripViewAlignment.Top;
+            ((Telerik.WinControls.UI.RadPageViewStripElement)(this.radPageView1.GetChildAt(0))).Text = "";
+            ((Telerik.WinControls.UI.RadPageViewStripElement)(this.radPageView1.GetChildAt(0))).StretchVertically = true;
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.lblHeader.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblHeader.Location = new System.Drawing.Point(332, 11);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(108, 39);
+            this.lblHeader.TabIndex = 1;
+            this.lblHeader.Text = "מחירון";
+            // 
+            // okBtn
+            // 
+            this.okBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.okBtn.Location = new System.Drawing.Point(701, 327);
+            this.okBtn.Name = "okBtn";
+            this.okBtn.Size = new System.Drawing.Size(75, 23);
+            this.okBtn.TabIndex = 2;
+            this.okBtn.Text = "אישור";
+            this.okBtn.UseVisualStyleBackColor = true;
+            this.okBtn.Click += new System.EventHandler(this.OKBtn_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(620, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "סגור";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // PriceList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.okBtn);
+            this.Controls.Add(this.radPageView1);
+            this.Controls.Add(this.lblHeader);
+            this.Name = "PriceList";
+            this.Size = new System.Drawing.Size(790, 366);
+            this.Tag = "W";
+            this.Resize += new System.EventHandler(this.WindowExtension_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.radPageView1)).EndInit();
+            this.radPageView1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadPageViewPage radPageViewPage6;
+        private Telerik.WinControls.UI.RadPageViewPage radPageViewPage5;
+        private Telerik.WinControls.UI.RadPageViewPage radPageViewPage4;
+        private Telerik.WinControls.UI.RadPageViewPage radPageViewPage3;
+        private Telerik.WinControls.UI.RadPageViewPage radPageViewPage2;
+        private Telerik.WinControls.UI.RadPageViewPage radPageViewPage1;
+        private Telerik.WinControls.UI.RadPageView radPageView1;
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.Button okBtn;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ PriceList/PriceList.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Common;
+using DAL;
+using LSExtensionWindowLib;
+using LSSERVICEPROVIDERLib;
+using Telerik.WinControls.UI;
+using Telerik.WinControls;
+
+namespace PriceList
+{
+
+    [ComVisible(true)]
+    [ProgId("PriceList.PriceList")]
+    public partial class PriceList : UserControl, IExtensionWindow
+    {
+        #region Ctor
+        public PriceList()
+        {
+            InitializeComponent();
+            BackColor = Color.FromName("Control");
+        }
+        #endregion
+
+        #region private members
+
+
+        private IExtensionWindowSite2 _ntlsSite;
+        private INautilusDBConnection _ntlsCon;
+        private List<TestTemplateEx> _TestTemplatelist;
+        private IDataLayer dal;
+
+        #endregion
+
+        #region Implementation of IExtensionWindow
+
+        public bool CloseQuery()
+        {
+            return true;
+        }
+
+        public void Internationalise() { }
+
+        public void SetSite(object site)
+        {
+            _ntlsSite = (IExtensionWindowSite2)site;
+            _ntlsSite.SetWindowInternalName("");
+            _ntlsSite.SetWindowRegistryName("");
+            _ntlsSite.SetWindowTitle("מחירון");
+        }
+
+
+        public void PreDisplay()
+        {
+            Utils.CreateConstring(_ntlsCon);
+            dal = new DataLayer();
+            dal.Connect();
+            PopulateTabs();
+        }
+
+        public WindowButtonsType GetButtons()
+        {
+            return LSExtensionWindowLib.WindowButtonsType.windowButtonsNone;
+        }
+
+        public bool SaveData()
+        {
+            return false; 
+        }
+
+        public void SetServiceProvider(object serviceProvider)
+        {
+            NautilusServiceProvider sp = serviceProvider as NautilusServiceProvider;
+            _ntlsCon = Utils.GetNtlsCon(sp);
+
+        }
+
+        public void SetParameters(string parameters)
+        {
+
+        }
+
+        public void Setup()
+        {
+
+        }
+
+        public WindowRefreshType DataChange()
+        {
+            return LSExtensionWindowLib.WindowRefreshType.windowRefreshNone;
+        }
+
+        public WindowRefreshType ViewRefresh()
+        {
+            return LSExtensionWindowLib.WindowRefreshType.windowRefreshNone;
+        }
+
+        public void refresh() { }
+
+        public void SaveSettings(int hKey) { }
+
+        public void RestoreSettings(int hKey) { }
+        public void Close()
+        {
+
+        }
+
+
+        #endregion
+
+        #region Init controls
+
+        private void PopulateTabs()
+        {
+            //get Test templates
+            _TestTemplatelist = dal.GetTestTemplatesForPriceList();
+
+            foreach (var tab in radPageView1.Pages)
+            {
+                //Create a Grid View for each TAB
+                CreateGridView(tab);
+
+                var radDataGrid = (RadGridView)tab.Controls[0];
+
+                //Add coulmns
+                AddColumns(radDataGrid);
+
+                DesignColumns(radDataGrid);
+
+                //get lab of tab
+                var labTab = tab.Tag.ToString();
+
+
+                if (labTab != "ALL")
+                {
+                    var newList = new List<TestTemplateEx>();
+
+                    foreach (var item in _TestTemplatelist)
+                    {
+                        if (item == null || item.RelevantLabs == null) continue;
+                        var labs = item.RelevantLabs.Split(',');
+
+                        bool containsThisLab = labs.Contains(labTab);
+                        if (containsThisLab)
+                        {
+                            newList.Add(item);
+                        }
+                    }
+
+                   
+                    radDataGrid.DataSource = newList;
+
+                }
+                else
+                {
+                    radDataGrid.DataSource = _TestTemplatelist;
+                }
+            }
+
+
+
+        }
+
+      
+
+        private void CreateGridView(RadPageViewPage tab)
+        {
+            var rgv = new RadGridView
+                          {
+                              AutoSizeColumnsMode = GridViewAutoSizeColumnsMode.Fill,
+                              EnableAlternatingRowColor = true,
+                              ShowRowErrors = true,
+                              Dock = DockStyle.Fill,
+                              AllowAddNewRow = false,
+                              RightToLeft = RightToLeft.Yes,
+                              ShowRowHeaderColumn = true,
+                              AllowDeleteRow = false,
+                              Width = 800,
+                              Height = 800,
+                              ReadOnly = false,
+                              AutoGenerateColumns = true
+                          };
+            rgv.TableElement.Padding = new Padding(0);
+            rgv.TableElement.DrawBorder = true;
+            rgv.TableElement.CellSpacing = -1;
+            rgv.TableElement.Text = "";
+
+     
+            rgv.RowFormatting += (rgv_RowFormatting);
+
+            tab.Controls.Add(rgv);
+        }
+
+        void rgv_RowFormatting(object sender, RowFormattingEventArgs e)
+        {
+
+            var tt = (TestTemplateEx)e.RowElement.RowInfo.DataBoundItem;
+            if (tt!=null&&tt.Price<=0)
+            {
+         
+            
+                e.RowElement.DrawFill = true;
+                e.RowElement.GradientStyle = GradientStyles.Solid;
+                e.RowElement.BackColor = Color.Red;
+            }
+            else
+            {
+                e.RowElement.ResetValue(LightVisualElement.BackColorProperty, ValueResetFlags.Local);
+                e.RowElement.ResetValue(LightVisualElement.GradientStyleProperty, ValueResetFlags.Local);
+                e.RowElement.ResetValue(LightVisualElement.DrawFillProperty, ValueResetFlags.Local);
+            }
+            
+
+            
+        }
+
+
+
+
+
+
+
+
+
+        private void AddColumns(RadGridView radDataGrid)
+        {
+            if (radDataGrid.ColumnCount == 0)
+            {
+
+
+                radDataGrid.Columns.Add(new GridViewTextBoxColumn("שם הבדיקה", "Workflow.Description"));
+
+                radDataGrid.Columns.Add(new SelectedPhraeGridViewTextBoxColumn("שם הבדיקה",
+                                                                                "Completion",
+                                                                                "Completion",
+                                                                                dal));
+
+                radDataGrid.Columns.Add(new GridViewTextBoxColumn("תקן", "Standard"));
+                radDataGrid.Columns.Add(new GridViewTextBoxColumn("זמן קבלה", "Completion"));
+                radDataGrid.Columns.Add(new GridViewCheckBoxColumn("הסמכה", "Authorization"));
+                radDataGrid.Columns.Add(new GridViewCheckBoxColumn("הכרה", "Recognition"));
+                radDataGrid.Columns.Add(new GridViewCheckBoxColumn("קבלן משנה", "Outsource"));
+                radDataGrid.Columns.Add(new GridViewDecimalColumn("מחיר", "Price"));
+
+                
+
+            }
+        }
+
+
+
+        private void DesignColumns(RadGridView radDataGrid)
+        {
+            foreach (var item in radDataGrid.Columns)
+            {
+                item.Width = 100;
+                item.TextAlignment = ContentAlignment.MiddleCenter;
+                item.ReadOnly = true;
+
+                if (item.Name == "מחיר")
+                    item.ReadOnly = false;
+            }
+        }
+
+
+        
+        #endregion
+
+        private void OKBtn_Click(object sender, EventArgs e)
+        {
+
+            dal.SaveChanges();
+            _ntlsSite.CloseWindow();
+
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+
+            if (HasChanges())
+            {
+                var dr = MessageBox.Show("האם ברצונך לשמור את השינויים?", "", MessageBoxButtons.YesNoCancel);
+
+                if (dr == DialogResult.Yes)
+                {
+                    dal.SaveChanges();
+                }
+                else if (dr == DialogResult.Cancel)
+                {
+                    return;
+                }
+            }
+            dal.Close();
+            _ntlsSite.CloseWindow();
+        }
+
+        private void WindowExtension_Resize(object sender, EventArgs e)
+        {
+            lblHeader.Location = new Point(Width / 2 - lblHeader.Width / 2, lblHeader.Location.Y);
+        }
+
+        private bool HasChanges()
+        {
+            return _TestTemplatelist.Any(item => item.EntityState == EntityState.Modified);
+        }
+    }
+
+    public class SelectedPhraeGridViewTextBoxColumn : GridViewTextBoxColumn
+    {
+        private string selectedPhraseName;
+        private string selectedPhraseDescription;
+        public SelectedPhraeGridViewTextBoxColumn() {}
+
+        public SelectedPhraeGridViewTextBoxColumn(string fieldName) : base(fieldName){}
+
+        public SelectedPhraeGridViewTextBoxColumn(string uniqueName, string fieldName) : base(uniqueName, fieldName){}
+
+        public SelectedPhraeGridViewTextBoxColumn(string uniqueName, string phraseHeader, string phraseName, IDataLayer dal) : base(uniqueName)
+        {
+            selectedPhraseName = dal.GetPhraseByName(phraseHeader).PhraseEntries.Where(pe => pe.PhraseName == phraseName).FirstOrDefault().PhraseName;
+            selectedPhraseDescription = dal.GetPhraseByName(phraseHeader).PhraseEntries.Where(pe => pe.PhraseName == phraseName).FirstOrDefault().PhraseDescription;
+        }
+
+        public string SelectedPhraseName
+        {
+            get { return selectedPhraseName; }
+        }
+
+        public string SelectedPhraseDescription
+        {
+            get { return selectedPhraseDescription; }
+        }
+
+
+    }
+
+
+
+}
+
+
+
+
